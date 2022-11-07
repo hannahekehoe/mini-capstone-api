18d8a4d1ca6227f4ebccdf0e73ada4c145ac7de7
hannahekehoe <hkehoe90@gmail.com>
2022-11-07 13:30:21 -0500

    added dicount, tax, and total model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,16 @@ class Product < ApplicationRecord
   def friendly_created_at
     created_at.strftime("%A, %d %b %Y %l:%M %p")
   end
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    tax = price * 0.08
+  end
+
+  def total
+    total = tax + price
+  end
 end
