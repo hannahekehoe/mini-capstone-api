ea6cc9088d466788f1d1fdc1c0db3f600e59445a
hannahekehoe <hkehoe90@gmail.com>
2022-11-10 13:48:39 -0500

    added web token and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
 
+  post "/sessions" => "sessions#create"
+
   # get "/fountain_pen", controller: "products", action: "fountain_pen"
 
   # get "/notebook", controller: "products", action: "notebook"
